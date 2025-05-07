06193d0d40c02e3e48206b43e59048d382301edb
Egor <kerminegor@gmail.com>
2025-05-07 11:09:01 +0300

    delete old

--- src/views/WikibotMainView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="container" style="height: 100%">
-    <TicketDetail />
-  </div>
-</template>
-<script setup>
-import TicketDetail from '../modules/ticketCentralBlock/views/TicketDetail.vue'
-</script>
