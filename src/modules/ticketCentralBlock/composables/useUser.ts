ffac642f5ead091ad0a449f4147a551edc369582
Egor <kerminegor@gmail.com>
2025-05-07 12:49:22 +0300

    fetching

--- src/modules/ticketCentralBlock/composables/useUser.ts
+++ src/modules/ticketCentralBlock/composables/useUser.ts
@@ -3,12 +3,18 @@ import type { UserEntityWithoutSecrets } from '@/services/helpfy/helpfy.schemas'
 import { ref } from 'vue'
 
 const user = ref<UserEntityWithoutSecrets>()
+const fetching = ref(false)
 
 export const useUser = () => {
-  if (!user.value) {
-    authControllerCheckSession().then((data) => {
-      user.value = data.data
-    })
+  if (!user.value && !fetching.value) {
+    fetching.value = true
+    authControllerCheckSession()
+      .then((data) => {
+        user.value = data.data
+      })
+      .finally(() => {
+        fetching.value = false
+      })
   }
 
   return {
