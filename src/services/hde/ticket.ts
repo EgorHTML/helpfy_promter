5b48d6d1b201d8a85f7d40a666d0ae40b1b70c9d
Egor <kerminegor@gmail.com>
2025-06-02 22:21:36 +0300

    ticket service

---
+++ src/services/hde/ticket.ts
@@ -0,0 +1,18 @@
+import request from '../request'
+
+export interface CreateTicketDTO {
+  title: string
+  description: string
+  user_id?: number
+}
+
+export function createTicket(ticketData: CreateTicketDTO) {
+  return request(`${window.location.origin}/api/v2/tickets/`, {
+    method: 'POST',
+    data: JSON.stringify(ticketData),
+  })
+    .then((data: any) => data.data)
+    .catch(() => {
+      console.warn('Не удалось создать заявку')
+    })
+}
