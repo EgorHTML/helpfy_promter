1137dd554a7e437c7b0a3b392a4224d81044d12f
Egor <kerminegor@gmail.com>
2025-06-03 13:32:25 +0300

    dynamic module

--- src/modules/ticketCentralBlock/plugins/pluginButton.ts
+++ src/modules/ticketCentralBlock/plugins/pluginButton.ts
@@ -1,8 +1,16 @@
-export function clickOnPluginButton(plugin: any) {
+export async function clickOnPluginButton(plugin: any) {
+  const module = await import('../composables/useEvaluation')
+  const { markSend } = module.useEvaluation()
   const pluginButton = window.parent.document
     ?.querySelector(`iframe[src="/custom/${plugin.cache}"]`)
     ?.closest('.ticket-plugins__dialog')
     ?.querySelector('.ticket-plugins__button') as HTMLElement
 
+  const prev = markSend.value
+  markSend.value = true
+
   pluginButton?.click()
+  setTimeout(() => {
+    markSend.value = prev
+  }, 200)
 }
